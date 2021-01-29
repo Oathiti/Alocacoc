493ef313f720b2bce0e3cae56b4a6faeaca8dcdc
Oat <thitipong@globalwireless.co.th>
2021-01-29 14:02:58 +0700

    update Movement

--- Assets/Scripts/Movement/Movement.cs
+++ Assets/Scripts/Movement/Movement.cs
@@ -5,22 +5,29 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public CharacterController characterController;
+
     public float spdWalk = 6f;
     public float spdRotation = 0.1f;
     public LayerMask layerMaskGround;
 
     float cVelocity = 0.1f;
-    const float gravity = -9.8f;
+    int jumpCount = 1;
+
     const float jumpHeight = 1.0f;
-    const float groundDistance = 0.1f;
+    const float groundDistance = 0.01f;
 
     [Space]
     [SerializeField]
     Vector3 playerVelocity;
+    [SerializeField]
+    Vector3 direction;
+    [SerializeField]
+    bool isGrounded;
 
     void Update()
     {
-        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+        isGrounded = Physics.CheckBox(transform.position, Vector3.one * groundDistance);
+        direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
 
         if (direction.magnitude >= 0.1f)
         {
@@ -36,28 +43,26 @@ public class Movement : MonoBehaviour
                 float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + Camera.main.transform.eulerAngles.y;
                 angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref cVelocity, spdRotation);
             }
-            direction = Camera.main.transform.TransformDirection(direction);
+            direction = Camera.main.transform.rotation * direction;
             direction.y = 0;
-
             transform.rotation = Quaternion.Euler(0, angle, 0);
-            characterController.Move(direction * spdWalk * Time.deltaTime);
+            direction *= spdWalk ;
+            //characterController.Move(direction * spdWalk * Time.deltaTime);
+
         }
-        if (characterController.isGrounded )
+        if (isGrounded && playerVelocity.y < 0)
         {
-            playerVelocity.y = 0;
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravity);
-            }
+            playerVelocity.y = 0f;
         }
-        else
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            playerVelocity.y += gravity * Time.deltaTime;
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y);
         }
-
-
-
-        characterController.Move(playerVelocity * Time.deltaTime);
-
+        playerVelocity.y += Physics.gravity.y * Time.deltaTime;
+        characterController.Move((playerVelocity + direction) * Time.deltaTime);
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireCube(transform.position, Vector3.one * groundDistance);
     }
 }
