f5c8e87b13bd4f710244b7897e5bc03eef172e96
nitpum <nitpum@outlook.com>
2021-01-29 23:17:32 +0700

    feat: Add patrol path

---
+++ Assets/Scripts/NPC/PatrolPath.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PatrolPath : MonoBehaviour
+{
+
+    public Transform[] points;
+
+    public Transform GetNextPoint(Transform currentPoint)
+    {
+        for (int i = 0; i < points.Length - 1; i++)
+        {
+            if (points[i] == currentPoint)
+            {
+                if(i < points.Length - 1)
+                {
+                    return points[i + 1];
+                }
+                else
+                {
+                    return points[0];
+                }
+            }
+        }
+
+        return null;
+    }    
+    
+}
