52b2ce37c170e6d2177634aa0b5851bd4c6c2ca0
Farm <Thunderock01@gmail.com>
2021-01-31 00:51:25 +0700

    Update

--- Assets/_Game/Scripts/NPC/NPCHateCat.cs
+++ Assets/_Game/Scripts/NPC/NPCHateCat.cs
@@ -1,11 +1,21 @@
 using UnityEngine;
+using System;
+using System.Collections;
 
 public class NPCHateCat : ReactToMeow
 {
 
     public override void MeowReact(GameObject invoker)
-    {  
+    {
         invoker.GetComponent<Movement>().Scare();
+        StartCoroutine(ScareCat());
+    }
+
+    IEnumerator ScareCat()
+    {
+        GetComponent<AIAnim>().encounterCat = true;
+        yield return new WaitForSeconds(3f);
+        GetComponent<AIAnim>().encounterCat = false;
     }
 
 }
