2dd73d18e388cfea1da63d830b470c89db523a81
nitpum <nitpum@outlook.com>
2021-01-30 21:41:35 +0700

    Update main menu animation

---
+++ Assets/_Game/Scripts/Menu/StartGame.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+
+    public GameObject catCamera;
+
+    public GameObject fadeIn;
+
+    public string playScene;
+
+    public void Play()
+    {
+        catCamera.SetActive(true);
+        fadeIn.SetActive(true);
+        StartCoroutine("GoToPlayScene");
+    }
+
+    protected IEnumerator GoToPlayScene()
+    {
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene(playScene);
+        yield return null;
+    }
+}
