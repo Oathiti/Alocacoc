45993fa5b627752e50288a73b8433d970d893ad4
Tam <t2spade@gmail.com>
2021-01-30 23:35:04 +0700

    score

---
+++ Assets/_Game/Scripts/ScoreController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreController : MonoBehaviour
+{
+    public float score;
+    bool onCount;
+    void Start()
+    {
+        StartScore();
+    }
+
+    public void StartScore()
+    {
+        StartCoroutine(CountScore());
+    }
+
+    public void EndScore()
+    {
+        onCount = false;
+    }
+
+    IEnumerator CountScore()
+    {
+        onCount = true;
+        while (onCount)
+        {
+            score += Time.deltaTime;
+            yield return null;
+        }
+    }
+}
