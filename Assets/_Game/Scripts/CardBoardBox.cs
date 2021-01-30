9596f4f884d5fcd291485e4c021584eae2bd8feb
Oat_Dev <thitipong@globalwireless.co.th>
2021-01-31 00:42:30 +0700

    update

---
+++ Assets/_Game/Scripts/CardBoardBox.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardBoardBox : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Movement movement;
+        if(other.TryGetComponent(out movement))
+        {
+            movement.StartCoroutine(movement.Box());
+            gameObject.SetActive(false);
+        }
+    }
+}
