ffd497c0c8a032e1a3ce4747087735c2ee4e7398
Tam <t2spade@gmail.com>
2021-01-31 12:15:34 +0700

    Tam

---
+++ Assets/_Game/Scripts/UiInput.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class UiInput : MonoBehaviour
+{
+    public KeyCode key;
+    public Image keyImg;
+    public Sprite activeKey;
+    Sprite defaulKey;
+
+    private void Start()
+    {
+        defaulKey = keyImg.sprite;
+    }
+
+    private void Update()
+    {
+        keyImg.sprite = Input.GetKey(key)? activeKey:defaulKey;
+    }
+
+}
