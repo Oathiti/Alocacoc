0d06d2b2a7ae2da944e0bb7e6a0b8807a286f050
Oat <thitipong@globalwireless.co.th>
2021-01-31 15:44:05 +0700

    Update GameManager.cs

--- Assets/_Game/Scripts/GameManager.cs
+++ Assets/_Game/Scripts/GameManager.cs
@@ -42,21 +42,22 @@ public class GameManager : MonoBehaviour
         {
             Instantiate(goodGuy[i], pointSpawn[i].position, goodGuy[i].transform.rotation).name = "NPC_Good";
         }
-        for (int i = 0; i < minimumSpawn; i++)
-        {
-            int ii;
-            Instantiate(badGuy[ii = Random.Range(0, badGuy.Length)], pointSpawn[i + goodGuy.Length].position, badGuy[ii].transform.rotation).name = "NPC_Bad";
-            badGuyCount++;
-        }
+        //for (int i = 0; i < minimumSpawn; i++)
+        //{
+        //    int ii;
+        //    Instantiate(badGuy[ii = Random.Range(0, badGuy.Length)], pointSpawn[i + goodGuy.Length].position, badGuy[ii].transform.rotation).name = "NPC_Bad";
+        //    badGuyCount++;
+        //}
         StartCoroutine(SpawnBadGuy());
     }
     IEnumerator SpawnBadGuy()
     {
         while (badGuyCount < badGuylimit)
         {
-            yield return new WaitForSeconds(5f);
+            yield return new WaitForSeconds(badGuyCount < minimumSpawn ? .1f : 5f);
             int i;
             Instantiate(badGuy[i = Random.Range(0, badGuy.Length)], pointSpawn[Random.Range(0, pointSpawn.Length)].position, goodGuy[i].transform.rotation);
+            badGuyCount++;
         }
     }
 
