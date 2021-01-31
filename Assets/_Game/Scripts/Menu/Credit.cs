ff9407521bb1db9ad241f1a292650372dc18ec99
nitpum <nitpum@outlook.com>
2021-01-31 16:00:07 +0700

    Update credit in main menu

---
+++ Assets/_Game/Scripts/Menu/Credit.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class Credit : MonoBehaviour
+{
+
+    public GameObject mainCamera;
+
+    public GameObject creditCamera;
+
+    public GameObject mainMenuUI;
+
+    public GameObject creditUI;
+
+    public Animation creditFadeAnimation;
+
+    public Animation credtScrollAnimation;
+
+    public void ShowCredit()
+    {
+        mainCamera.SetActive(false);
+        creditCamera.SetActive(true);
+        creditUI.SetActive(true);
+        mainMenuUI.SetActive(false);
+
+        creditFadeAnimation.Play();
+        credtScrollAnimation.Play();
+    }
+
+    public void HideCredit()
+    {
+        mainCamera.SetActive(true);
+        creditCamera.SetActive(false);
+        creditUI.SetActive(false);
+        mainMenuUI.SetActive(true);
+    }
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Escape))
+        {
+            HideCredit();
+        }
+    }
+
+}
