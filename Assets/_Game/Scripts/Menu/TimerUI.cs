dba15160f9e0d75c79de30b159cab342bd8fe9b3
Oat_Dev <thitipong@globalwireless.co.th>
2021-01-31 12:52:03 +0700

    update

--- Assets/_Game/Scripts/Menu/TimerUI.cs
+++ Assets/_Game/Scripts/Menu/TimerUI.cs
@@ -1,6 +1,12 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimerUI : MonoBehaviour
 {
-    
+    public Text textTime;
+
+    private void Update()
+    {
+        textTime.text = ScoreController.score.ToString("0.00");
+    }
 }
