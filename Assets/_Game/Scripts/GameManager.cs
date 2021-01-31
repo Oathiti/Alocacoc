95352dd83b4c796471c30fa0e84f8fab13e2d9f2
Oat <thitipong@globalwireless.co.th>
2021-01-31 14:21:25 +0700

    Gamanage

---
+++ Assets/_Game/Scripts/GameManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameUtility;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] goodGuy;
+    public GameObject[] badGuy;
+
+    public Transform[] pointSpawn;
+
+    public int minimumSpawn;
+    public int badGuylimit;
+    int badGuyCount = 0;
+
+    public FollowerList followerList;
+
+    void Start()
+    {
+        followerList.onCount = (c) =>
+          {
+              if (c == goodGuy.Length)
+                  ShowScore();
+          };
+        goodGuy = goodGuy.SwapArray();
+        pointSpawn = pointSpawn.SwapArray();
+
+        for (int i = 0; i < goodGuy.Length; i++)
+        {
+            Instantiate(goodGuy[i], pointSpawn[i].position, goodGuy[i].transform.rotation);
+        }
+        for (int i = 0; i < goodGuy.Length; i++)
+        {
+            Instantiate(badGuy[Random.Range(0, badGuy.Length)], pointSpawn[i + goodGuy.Length].position, goodGuy[i].transform.rotation);
+            badGuyCount++;
+        }
+        StartCoroutine(SpawnBadGuy());
+    }
+    IEnumerator SpawnBadGuy()
+    {
+        while (badGuyCount < badGuylimit)
+        {
+            yield return new WaitForSeconds(5f);
+            int i;
+            Instantiate(badGuy[i = Random.Range(0, badGuy.Length)], pointSpawn[Random.Range(0, pointSpawn.Length)].position, goodGuy[i].transform.rotation);
+        }
+    }
+
+    void ShowScore()
+    {
+
+    }
+}
---
+++ Assets/_Game/Scripts/Swap.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+namespace GameUtility
+{
+
+    public static class Swap
+    {
+        /* Sample
+            array.SwapArray ();
+        */
+        public static T[] SwapArray<T>(this T[] arr)
+        {
+            if (arr.Length > 0)
+            {
+                System.Random random = new System.Random();
+
+                int rand = 0;
+                T temp;
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    rand = random.Next(0, arr.Length);
+                    temp = arr[i];
+                    arr[i] = arr[rand];
+                    arr[rand] = temp;
+                }
+            }
+            return arr;
+        }
+
+        /* Sample
+            arrayList.SwapArray ();
+        */
+        public static ArrayList SwapArrayList<T>(this T arr) where T : ArrayList
+        {
+            if (arr.Count > 0)
+            {
+                System.Random random = new System.Random();
+                int rand = 0;
+                object temp;
+                for (int i = 0; i < arr.Count; i++)
+                {
+                    rand = random.Next(0, arr.Count);
+                    temp = arr[i];
+                    arr[i] = arr[rand];
+                    arr[rand] = temp;
+                }
+            }
+            return arr;
+        }
+
+        /* Sample
+            list.SwapArray ();
+        */
+        public static List<T> SwapList<T>(this List<T> arr)
+        {
+            if (arr.Count > 0)
+            {
+                System.Random random = new System.Random();
+                int rand = 0;
+                T temp;
+                for (int i = 0; i < arr.Count; i++)
+                {
+                    rand = random.Next(0, arr.Count);
+                    temp = arr[i];
+                    arr[i] = arr[rand];
+                    arr[rand] = temp;
+                }
+            }
+            return arr;
+        }
+
+        public static T[] InsertArray<T>(this T[] arr, T[] array, int index)
+        {
+            Array.Resize(ref arr, arr.Length + array.Length);
+            for (int i = arr.Length - 1; i >= index + array.Length; i--)
+            {
+                arr[i] = arr[i - array.Length];
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                arr[i + index] = array[i];
+            }
+            return arr;
+        }
+    }
+
+}
